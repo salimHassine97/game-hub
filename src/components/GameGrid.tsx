a60cd395e1c589e9470b3400e0d7eaddfd71f500
Salim Hassine <salim.hassine@etu.univ-amu.fr>
2024-09-26 00:03:01 +0200

    création de la carte de jeu et les afficher dans le main

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import useGames from "../hooks/useGames";
+import { SimpleGrid, Text } from "@chakra-ui/react";
+import GameCard from "./GameCard";
 
 const GameGrid = () => {
   const { games, error, isLoading } = useGames();
+  if (error) return <Text>{error}</Text>;
   return (
-    <ul>
-      {games.map((game) => (
-        <li key={game.id}>{game.name}</li>
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      padding="10px"
+      spacing={6}
+    >
+      {games.map((g) => (
+        <GameCard game={g} key={g.id} />
       ))}
-    </ul>
+    </SimpleGrid>
   );
 };
 
