30f973b909a600c39979890fe05489573c5b6eb5
Salim Hassine <salim.hassine@etu.univ-amu.fr>
2024-09-27 16:29:00 +0200

    Affichage d'un spinner lors de chargement de la page

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,25 +1,30 @@
 import React from "react";
 import UseGenres from "../hooks/useGenres";
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import getCroppedImageUrl from "../services/getCroppedImageUrl";
 
 const GenreList = () => {
-  const { data } = UseGenres();
+  const { data, isLoading, error } = UseGenres();
+
+  if (error) return null;
   return (
-    <List>
-      {data.map((g) => (
-        <ListItem key={g.id} paddingY="5px">
-          <HStack>
-            <Image
-              boxSize="32px"
-              src={getCroppedImageUrl(g.image_background)}
-              borderRadius={8}
-            />
-            <Text fontSize="lg">{g.name}</Text>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      {isLoading && <Spinner />}
+      <List>
+        {data.map((g) => (
+          <ListItem key={g.id} paddingY="5px">
+            <HStack>
+              <Image
+                boxSize="32px"
+                src={getCroppedImageUrl(g.image_background)}
+                borderRadius={8}
+              />
+              <Text fontSize="lg">{g.name}</Text>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
