eab5efb65b124d3ce536b6551d5857a3cc7e302f
Salim Hassine <salim.hassine@etu.univ-amu.fr>
2024-09-27 16:22:27 +0200

    ajout des images pour la liste de genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 import UseGenres from "../hooks/useGenres";
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import getCroppedImageUrl from "../services/getCroppedImageUrl";
 
 const GenreList = () => {
   const { data } = UseGenres();
   return (
-    <ul>
+    <List>
       {data.map((g) => (
-        <li key={g.id}>{g.name}</li>
+        <ListItem key={g.id} paddingY="5px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              src={getCroppedImageUrl(g.image_background)}
+              borderRadius={8}
+            />
+            <Text fontSize="lg">{g.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
